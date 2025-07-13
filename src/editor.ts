d23aca7ddbaaa23be8861abaa28285ef77c153c0
kxxt <rsworktech@outlook.com>
2025-07-13 13:56:07 +0800

    Add example language and sidebar  for choosing language

--- src/editor.ts
+++ src/editor.ts
@@ -2,14 +2,14 @@ import { EditorView, basicSetup } from "codemirror"
 import { keymap } from "@codemirror/view"
 import { indentWithTab } from "@codemirror/commands"
 import { acceptCompletion } from "@codemirror/autocomplete"
-import { javascript } from "@codemirror/lang-javascript"
 import { treeView } from '@overleaf/codemirror-tree-view'
-import type { Extension } from "@codemirror/state"
+import { EditorState, type EditorStateConfig, type Extension } from "@codemirror/state"
 
-export function setupEditor(parent: HTMLElement, extraExtensions?: [Extension]) {
-    let extra = extraExtensions ?? [];
+export function setupEditor(parent: HTMLElement, state: EditorStateConfig) {
+    let ext = state.extensions ? [state.extensions] : []
+    state.extensions = [basicSetup, keymap.of([{ key: "Tab", run: acceptCompletion }, indentWithTab]), treeView as Extension, ...ext];
     return new EditorView({
-        extensions: [basicSetup, keymap.of([{ key: "Tab", run: acceptCompletion }, indentWithTab]), javascript(), treeView, ...extra],
+        state: EditorState.create(state),
         parent
     })
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,50 @@
 import './style.css'
 import { setupEditor } from './editor.ts'
+import { javascript } from "@codemirror/lang-javascript"
+import { Compartment } from "@codemirror/state"
 import type { EditorView } from 'codemirror';
+import {
+  LanguageSupport,
+  LRLanguage,
+} from '@codemirror/language'
+import { parser } from './parser.mjs'
 
-globalThis.editor = setupEditor(document.getElementById('editor')!);
+let lang = new LanguageSupport(LRLanguage.define({
+  name: 'lang',
+  parser: parser
+}));
+
+let LANGUAGES: { [language_id: string]: LanguageSupport; } = {
+  "lang": lang,
+  "javascript": javascript()
+}
+
+let language = new Compartment;
+
+globalThis.editor = setupEditor(document.getElementById('editor')!, {
+  extensions: [
+    language.of(lang)
+  ]
+});
+
+let languageSelect = document.getElementById("language")! as HTMLSelectElement;
+for (const languageId in LANGUAGES) {
+  let option = document.createElement("option");
+  option.innerText = languageId;
+  languageSelect.appendChild(
+    option
+  )
+}
+languageSelect.addEventListener("change", (ev) => {
+  let select = ev.target as HTMLSelectElement;
+  setLanguage(select.options[select.selectedIndex].value)
+})
+
+function setLanguage(lang: string) {
+  editor.dispatch({
+    effects: language.reconfigure(LANGUAGES[lang])
+  })
+}
 
 declare global {
   var editor: EditorView
